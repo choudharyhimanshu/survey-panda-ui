bf62d581befcd2c1469f2dfedb31fb23df41a898
Himanshu Choudhary <himanshuchoudhary@live.com>
2019-10-15 01:24:22 +0530

    - Updated view page to under construction

--- src/routes/view/ViewSurvey.tsx
+++ src/routes/view/ViewSurvey.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Container, Grid} from 'semantic-ui-react';
+import {Container, Grid, Header, Icon} from 'semantic-ui-react';
 import {IGlobalContext, withGlobalContext} from '../../shared/contexts/global.context';
 
 export interface IViewProps {
@@ -18,8 +18,14 @@ class ViewSurvey extends React.Component<IViewProps, {}> {
             <Container className='pt-10'>
                 <Grid>
                     <Grid.Row>
-                        <Grid.Column>
-                            <h2>View Survey</h2>
+                        <Grid.Column className='pt-10 pb-10' textAlign='center'>
+                            <Header as='h2' icon>
+                                <Icon name='settings' />
+                                Under Construction!
+                                <Header.Subheader>
+                                    Request your patience. :)
+                                </Header.Subheader>
+                            </Header>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
